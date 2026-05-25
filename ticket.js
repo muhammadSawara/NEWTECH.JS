a494deb12640e97630970207fd3addaf8cdc014a
muhammadSawara <muhammadsawara2001@gmail.com>
2026-05-25 20:29:07 +0300

    solved

---
+++ ticket.js
@@ -0,0 +1,18 @@
+let age;
+if(age<5){
+    console.log("entrence is free for u");
+}
+
+else if(age>=5 && age<=17){
+    console.log("pay 10 dollars");
+}
+
+else if(age>=18 && age<=64){
+    console.log("pay 20 dollars");
+}
+
+else{
+    console.log("pay 12 dollars");
+    
+}
+    
