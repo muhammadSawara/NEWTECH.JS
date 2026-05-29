f32b76a30df3687e3e572abea861af55816e1d0a
muhammadSawara <muhammadsawara2001@gmail.com>
2026-05-29 14:20:21 +0300

    solved

---
+++ discount.js
@@ -0,0 +1,16 @@
+let cartTotal = Number(prompt("Enter cart total"));
+let member = prompt("Are you a member?")
+
+if(member == "yes" && cartTotal>=200){
+    cartTotal= cartTotal*80/100;
+}
+
+else if(member == "yes" && cartTotal<200){
+    cartTotal = cartTotal*90/100;
+}
+
+else if(member == "no" && cartTotal>=200){
+    cartTotal = cartTotal*95/100;
+}
+ 
+console.log("pay " + cartTotal + " dollars");
