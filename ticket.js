094d7eaa623021b4d7bd8520b8f6852f234e0adc
muhammadSawara <muhammadsawara2001@gmail.com>
2026-05-29 13:35:25 +0300

    did some changes

--- ticket.js
+++ ticket.js
@@ -1,18 +1,23 @@
-let age;
-if(age<5){
-    console.log("entrence is free for u");
+let age = Number(prompt("enter your age"));
+let price;
+if(age<0){
+    console.log("wrong input");
 }
-
+else if(age>0 && age<5){
+    price=0;
+}
 else if(age>=5 && age<=17){
-    console.log("pay 10 dollars");
+    price=10;
 }
 
 else if(age>=18 && age<=64){
-    console.log("pay 20 dollars");
+    price=20;
 }
 
 else{
-    console.log("pay 12 dollars");
-    
+    price=12;    
 }
+
+console.log("pay "+ price + " dollars");
+
     
